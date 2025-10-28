f446439f80074e1c08fe9adc48c6a71cda2956b2
Kirill001-cyber <kirill.kulishov.00@mail.ru>
2025-10-28 11:19:06 +0300

    Add files via upload

---
+++ Pr2/src/main/java/com/mycompany/pr2/model/Person.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.pr2.model;
+
+/**
+ *
+ * @author Student
+ */
+public class Person {
+    
+    private String name;
+    
+    public Person(String value) {
+        name = value;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+}
---
+++ Pr2/src/main/java/com/mycompany/pr2/model/product.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.pr2.model;
+
+/**
+ *
+ * @author Student
+ */
+public class product {
+private String name;
+private String nameproduct;
+private String categoria;
+private int price;
+ 
+}
